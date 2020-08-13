f591710d7fb83dd6a588f13a5219612f4d826406
Adminext <mdgaorui@buaa.edu.cn>
2020-08-13 22:22:50 +0800

    081322更新
    终于把陈年footer给删了
    teaminfo的样式现在变得很好看

--- src/components/team/TeamInfo.vue
+++ src/components/team/TeamInfo.vue
@@ -8,18 +8,22 @@
       <a-divider>成员列表</a-divider>
       <memberList :groupid="groupObj.groupid"></memberList>
       <a-divider>详细信息</a-divider>
-      <a-descriptions>
-        <a-descriptions-item label="团队ID">
+      <a-descriptions layout="vertical" bordered>
+        <a-descriptions-item label="团队ID" span="2">
           {{groupObj.groupid}}
         </a-descriptions-item>
-        <a-descriptions-item label="建立时间">
+        <a-descriptions-item label="建立时间" span="2">
           {{groupObj.createdtime}}
         </a-descriptions-item>
-        <a-descriptions-item label="描述">
+        <a-descriptions-item label="描述" span="2">
           {{groupObj.description}}
         </a-descriptions-item>
         
       </a-descriptions>
+      <a-divider>团队管理</a-divider>
+      <a-button type="primary" block style="margin-top:10px">邀请成员加入团队</a-button>
+      <a-button type="primary" block style="margin-top:10px">管理成员权限</a-button>
+      <a-button type="danger" block style="margin-top:10px;margin-bottom=10px">解散团队</a-button>
     </div>
   </div>
 </template>
@@ -61,6 +65,6 @@ export default {
 
 #grouptitle{
   margin-top:10px;
-  font-size:50px;
+  font-size:24px;
 }
 </style>
