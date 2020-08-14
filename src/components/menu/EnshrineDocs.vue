63fc52bbcb066bfe732f1211d01c7d9a90902c0a
Adminext <mdgaorui@buaa.edu.cn>
2020-08-14 22:21:05 +0800

    031422更新
    组件化文档卡片，并在四个用到文档卡片的地方改成了引用文档卡片的样式

--- src/components/menu/EnshrineDocs.vue
+++ src/components/menu/EnshrineDocs.vue
@@ -1,13 +1,47 @@
 <template>
-    <div>
-        
-        <a-empty />
+    <div>    
+    <a-list :grid="{ gutter: 25, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 6 }" :data-source="data">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <docCard :docObj="item"></docCard>
+      </a-list-item>
+    </a-list>
     </div>
 </template>
 <script type="text/ecmascript-6">
-    export default {
-        data(){
-            return {}
-        }
+import docCard from '../docs/docCard.vue'
+const data = [
+  {
+    title: '今日头条',
+  },
+  {
+    title: '啊这',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },
+  {
+    title: 'Title 5',
+  },
+  {
+    title: 'Title 6',
+  },
+];
+export default {
+  components: {
+    docCard,
+  },
+  data(){
+    return {
+      data,
     }
-</script>
+  },
+  methods: {
+    toDocs() {
+      this.$router.push('/docs/1');
+    }
+  }
+}
+</script><style></style>
--- src/components/team/groupdocs.vue
+++ src/components/team/groupdocs.vue
@@ -1,27 +1,9 @@
 <template>
       <div>
         <a-col span="18">
-            <a-list :grid="{ gutter: 25, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 6 }" :data-source="data">
+          <a-list :grid="{ gutter: 25, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 6 }" :data-source="data">
             <a-list-item slot="renderItem" slot-scope="item">
-              
-              <a-card hoverable style="width: 190px" :title="item.title" @click="toDocs()">
-            <img
-              slot="cover"
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-            />
-            <template slot="actions" class="ant-card-actions">
-              <a-icon key="setting" type="setting" />
-              <a-icon key="edit" type="edit" />
-              <a-icon key="ellipsis" type="ellipsis" />
-            </template>
-            <a-card-meta title="Card title" description="This is the description">
-              <a-avatar
-                slot="avatar"
-                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              />
-            </a-card-meta>
-          </a-card>
+              <docCard :docObj="item"></docCard>
             </a-list-item>
           </a-list>
         </a-col>
@@ -33,6 +15,7 @@
 </template>
 <script type="text/ecmascript-6">
 import TeamInfo from './TeamInfo.vue';
+import docCard from '../docs/docCard.vue';
 const data = [
   {
     title: '啊',
@@ -57,6 +40,7 @@ const data = [
     export default {
       components: {
         TeamInfo,
+        docCard,
       },
         data(){
             return {
