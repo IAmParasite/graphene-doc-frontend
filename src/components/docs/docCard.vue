21248d8369abf15b5370361fe91a43e025c5bd2c
IAmParasite <1004183196@qq.com>
2020-08-16 23:44:30 +0800

    Revert "增加了团队邀请成员功能"
    This reverts commit a017bed6d0a0362e7990905b461e3dac801411a0.

--- src/components/docs/docCard.vue
+++ src/components/docs/docCard.vue
@@ -8,10 +8,10 @@
         @click="toDocs(docObj.id)"
       />
       <template slot="actions" class="ant-card-actions">
-        <a-icon type="delete" @click="deleteDocs(1)" v-if="fav!=2"/>
+        <a-icon type="delete" @click="confirmDelete(1)" v-if="fav!=2"/>
         <a-icon type="unlock" @click="revoverDoc()" v-if="fav==2"/>
         <a-icon type="edit" @click="showModal()" v-if="fav!=2"/>
-        <a-icon type="delete" @click="deleteDocs(2)" v-if="fav==2"/>
+        <a-icon type="delete" @click="confirmDelete(2)" v-if="fav==2"/>
         <a-icon type="file-add" @click="addFavorDocs()" v-if="fav==0" />
         <a-icon type="minus-square" @click="delFavorDocs()" v-if="fav==1" />
         
@@ -91,6 +91,8 @@ export default {
   },
 
   methods: {
+    
+
     successmsg(message) {
       this.$message.success(message);
     },
@@ -101,14 +103,12 @@ export default {
       //这边判断是否能看，比如occupied
       this.$router.push("/docs2/" + id);
     },
-    deleteDocs(flag) {
-      console.log("删除该项" + this.form.DocumentID);
-      var _this=this;
+
+    deleteDocs(flag,self) {
+      console.log("删除该项" + self.form.DocumentID);
       let formData = new FormData();
-      formData.append("DocumentID", this.form.DocumentID);
-      console.log(this.form)
+      formData.append("DocumentID", self.form.DocumentID);
       formData.append("username", localStorage.getItem("token"));
-      console.log(localStorage.getItem("token"));
       let config = {
         headers: {
           "Content-Type": "multipart/form-data",
@@ -119,16 +119,16 @@ export default {
         .then(function (response) {
           console.log(response.data.message);
           if (response.data.message == "success") {
-            _this.successmsg("删除成功");
+            self.successmsg("删除成功");
             setTimeout(() => {
               myrefresh();
             }, 2000);
           } else {
-            _this.errormsg("删除失败，请尝试刷新后重试");
+            self.errormsg("删除失败，请尝试刷新后重试");
           }
         })
         .catch(function () {
-          _this.errormsg("删除失败，请尝试刷新后重试");
+          self.errormsg("删除失败，请尝试刷新后重试");
         });
       }
       else {
@@ -136,19 +136,37 @@ export default {
           .then(function (response) {
             console.log(response.data.message);
             if (response.data.message == "success") {
-              _this.successmsg("彻底删除成功");
+              self.successmsg("彻底删除成功");
               setTimeout(() => {
                 myrefresh();
               }, 2000);
             } else {
-              _this.errormsg("彻底删除失败，请尝试刷新后重试");
+              self.errormsg("彻底删除失败，请尝试刷新后重试");
             }
           })
           .catch(function () {
-            _this.errormsg("彻底删除失败，请尝试刷新后重试");
+            self.errormsg("彻底删除失败，请尝试刷新后重试");
           });
       }
       
+    },
+
+    confirmDelete(x) {
+      var _this=this;
+      this.$confirm({
+        title: <div style="font-weight:bold">确认删除？</div>,
+        content: (x==1)?<div style="color:red;">文件将被移入回收站</div>:<div style="color:red;">文件将<span style="font-weight:bold"> 永 远 消 失 ！</span></div>,
+        okText: '删除',
+        okType: 'danger',
+        cancelText: '取消',
+        onOk() {
+          console.log('OK')
+          _this.$options.methods.deleteDocs(x, _this);
+        },
+        onCancel() {
+          console.log('Cancel');
+        },
+      });
     },
 
     addFavorDocs() {
